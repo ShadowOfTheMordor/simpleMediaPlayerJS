6fa41145bbfad65cdf5f283bf1530eb328a79373
Maxim Roschin <mroschin2@yandex.ru>
2020-12-23 23:54:49 +0300

    some additions to videoplayer

--- scripts/videoPlayer.js
+++ scripts/videoPlayer.js
@@ -17,6 +17,15 @@ export const videoPlayerInit = () => {
     const videoTimePassed = document.querySelector(".video-time__passed");
     const videoProgress = document.querySelector(".video-progress");
     const videoTimeTotal = document.querySelector(".video-time__total");
+    const videoVolumeDown = document.querySelector(".video-volume__down");
+    const videoVolumeUp = document.querySelector(".video-volume__up");
+    const videoVolume = document.querySelector(".video-volume");
+    const videoVolumeOff = document.querySelector(".video-volume__off");
+    const videoFullscreen = document.querySelector(".video-fullscreen");
+
+
+    let savedVolume = 0;
+    
 
     const toggleButtonIcon = () => {
         if (videoPlayer.paused) {
@@ -30,6 +39,8 @@ export const videoPlayerInit = () => {
     };
 
     const toggleVideoPlay = () => {
+        if (document.fullscreenElement === videoPlayer)
+            return;
         if (videoPlayer.paused) {
             videoPlayer.play();
         } else {
@@ -40,9 +51,19 @@ export const videoPlayerInit = () => {
     const stopPlay = () => {
         videoPlayer.pause();
         videoPlayer.currentTime = 0;
-    }
+    };
 
     const addZeroToTime = (number) => number < 10 ? '0' + String(number) : String(number);
+
+    const changeVideoVolume = (curVolumeRange, maxVolumeRange) => {
+        const volume = curVolumeRange / maxVolumeRange;
+        // console.log("cur vol = " + curVolumeRange + " maxVolume = " + maxVolumeRange);
+        // console.log(" video volume = " + volume);
+        videoPlayer.volume = volume;
+        // console.log(" video volume = " + videoPlayer.volume);
+        // const volumeRange = volume*100;
+        // videoVolume.value = curVolumeRange;
+    };
 
     videoPlayer.addEventListener("click", toggleVideoPlay);
     videoButtonPlay.addEventListener("click", toggleVideoPlay);
@@ -75,12 +96,73 @@ export const videoPlayerInit = () => {
         videoTimeTotal.textContent = `${addZeroToTime(minutesTotal)}:${addZeroToTime(secondsTotal)}`;
     });
 
-    videoProgress.addEventListener("change", () => {
+    videoProgress.addEventListener("input", () => {
         const duration = videoPlayer.duration;
         const progressValue = videoProgress.value;
         // console.log(" progress = " + progressValue);
         videoPlayer.currentTime = (progressValue * duration) / 100;
     });
+
+    videoVolume.addEventListener("input", () => {
+        const maxVolumeRange = videoVolume.max;
+        const currentVolumeRange = videoVolume.value;
+        changeVideoVolume(currentVolumeRange, maxVolumeRange);
+        // videoPlayer.volume = currentVolume / maxVolume;
+    });
+
+    videoVolumeDown.addEventListener("click", () => {
+        const maxVolumeRange = videoVolume.max;
+        if (savedVolume < 0.05) {
+            savedVolume = videoPlayer.volume;
+            changeVideoVolume(0, maxVolumeRange);
+            videoVolume.value = 0;
+
+        } else {
+            console.log(" saved volume = " + savedVolume);
+            changeVideoVolume(savedVolume*100, maxVolumeRange);
+            videoVolume.value = savedVolume*100;
+            savedVolume = 0;
+        }
+    });
+
+    videoVolumeUp.addEventListener("click", () => {
+        const maxVolumeRange = videoVolume.max;
+        if (savedVolume < 0.05) {
+            savedVolume = videoPlayer.volume;
+            changeVideoVolume(maxVolumeRange, maxVolumeRange);
+            videoVolume.value = maxVolumeRange;
+        } else {
+            changeVideoVolume(savedVolume*100, maxVolumeRange);
+            videoVolume.value = savedVolume*100;
+            savedVolume = 0;
+        }
+    });
+
+    videoVolumeOff.addEventListener("click", () => {
+        const maxVolumeRange = videoVolume.max;
+        // if (videoPlayer.volume < 0.1) {
+         if (savedVolume > 0.05) {
+            changeVideoVolume(savedVolume*100, maxVolumeRange);
+            savedVolume = 0;
+        } else {
+            savedVolume = videoPlayer.volume;
+            changeVideoVolume(0, maxVolumeRange);
+        }
+    });
+
+    videoFullscreen.addEventListener("click", () => {
+        videoPlayer.requestFullscreen();
+    });
+
+    videoPlayer.addEventListener("volumechange", () => {
+        videoVolume.value = videoPlayer.volume * 100;
+    });
+
+    
+    
+    //for setting starting volume
+    changeVideoVolume(videoVolume.value, videoVolume.max);
+
 };
 
 
